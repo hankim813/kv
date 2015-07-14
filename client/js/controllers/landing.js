13c395d7f3dcabb5ecee07d55a019b723bd8c2bf
Han Kim <han@segment.com>
2015-07-14 00:10:52 -0700

    css fixes, active links

--- client/js/controllers/landing.js
+++ client/js/controllers/landing.js
@@ -3,10 +3,17 @@
 angular
 	.module('kvApp')
 
-	.controller('LandingController', ['$window', function($window) {
+	.controller('LandingController', ['$window', '$location', function($window, $location) {
 		var vm = this;
 
-		// Vertical centering
+		// Vertical Centering
 		var parent = document.getElementById('parent');
 		parent.style.height = $window.innerHeight + 'px';
+
+		// Active Link Init
+		vm.isActiveAnchor = isActiveAnchor;
+
+		function isActiveAnchor (currentAnchor) {
+			return $location.path() === currentAnchor;
+		};
 	}]);
